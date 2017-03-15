e98f685a80ba64f2b3e198074b8f413a9330fa54
Krasimir Georgiev <krasi2405@gmail.com>
2017-03-15 02:25:35 +0200

    Added base app. User can add items, remove all items and remove an item by index.

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import  (Flask, render_template, make_response,
+					redirect, request, url_for, flash)
+
+import json
+
+import pdb
+
+users = []
+
+app = Flask(__name__)
+app.secret_key = "ofsajpiofjeiwjg09w0ejgfajodf9hew809g092uja012-$90ash"
+
+def get_saved_data():
+	try:
+		data = json.loads(request.cookies.get('list_info'))
+	except TypeError:
+		data = []
+	return data
+
+@app.route("/")
+@app.route("/index")
+def index():
+	data = get_saved_data()
+	return render_template("index.html", data = data)
+
+@app.route("/save", methods = ["POST"])
+def save():
+	data = get_saved_data()
+
+	data.append(dict(request.form.items()))
+
+	response = make_response(redirect(url_for("index")))
+	response.set_cookie('list_info', json.dumps(data))
+	return response
+
+@app.route("/delete_all", methods = ["POST"])
+def delete_all():
+	data = []
+	response = make_response(redirect(url_for("index")))
+	response.set_cookie('list_info', json.dumps(data))
+	return response
+
+@app.route("/delete_item", methods = ["POST"])
+def delete_item():
+	data = get_saved_data();
+	try: 
+		index = int(request.form["index"])
+		if(index < 1 or index > len(data)):
+			flash("Element doesnt exist!")
+		else:
+			data.pop(index - 1)
+	except ValueError:
+		flash("Enter a value!");
+	
+
+	response = make_response(redirect(url_for("index")))
+	response.set_cookie('list_info', json.dumps(data))
+	return response;
+
+app.run(debug = True)
